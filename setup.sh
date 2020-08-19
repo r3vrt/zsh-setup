37a9448b9204114e3e76a6f0d46e05c126af58a5
r3vrt <r3vrt@huntbawks.home>
2020-08-19 14:41:06 +0100

    minor tweak

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 sed --in-place 's,ZSH_THEME="robbyrussell",ZSH_THEME="powerlevel10k/powerlevel10k",g' ${HOME}/.zshrc
+
+curl https://raw.githubusercontent.com/r3vrt/zsh-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
