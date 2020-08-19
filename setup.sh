226bee7155d773c8f2bf12a8d2ce6080b61c0784
r3vrt <r3vrt@huntbawks.home>
2020-08-19 14:51:07 +0100

    tweak

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 sed --in-place 's,ZSH_THEME="robbyrussell",ZSH_THEME="powerlevel10k/powerlevel10k",g' ${HOME}/.zshrc
 
 curl https://raw.githubusercontent.com/r3vrt/zsh-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
+
+echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ${HOME}/.zshrc
