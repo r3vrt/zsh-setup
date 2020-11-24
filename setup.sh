597807614de8f0bb6b363839f568ca6093d45f80
r3vrt <61834286+r3vrt@users.noreply.github.com>
2020-11-24 10:03:00 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 sed --in-place 's,ZSH_THEME="robbyrussell",ZSH_THEME="powerlevel10k/powerlevel10k",g' ${HOME}/.zshrc
 
 curl https://raw.githubusercontent.com/r3vrt/zsh-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
+
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ${HOME}/.zshrc
