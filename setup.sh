3c140f357884891cb972573e7db5e833e5495f9a
r3vrt <61834286+r3vrt@users.noreply.github.com>
2021-08-05 21:27:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-sed --in-place 's,ZSH_THEME="robbyrussell",ZSH_THEME="powerlevel10k/powerlevel10k",g' ${HOME}/.zshrc
+sed --in-place 's,ZSH_THEME="robbyrussell",ZSH_THEME="powerlevel10k/powerlevel10k",g' ~/.zshrc
 
-curl https://raw.githubusercontent.com/r3vrt/zsh-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
+curl https://raw.githubusercontent.com/r3vrt/zsh-setup/master/.p10k.zsh > ~/.p10k.zsh
 
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ${HOME}/.zshrc
+echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
